46851ae9145e1bb060a1d927007a2e59c5fca664
mohadesekiani <mohadeskiani6@gmail.com>
2023-08-12 16:51:52 +0330

    interfac sans edited

--- src/app/core/shema/models/Isans.ts
+++ src/app/core/shema/models/Isans.ts
@@ -1,15 +1,7 @@
 export interface Isans {
-  
   id: number;
   img: string;
   title: string;
   typeMovies: string;
-  sans: [
-    {
-      id: number;
-      hall: string;
-      money: string;
-      time: string;
-    }
-  ];
+  sans: Array<{}>;
 }
